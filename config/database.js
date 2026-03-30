fe6bd031ec3850a10186ccc109a89b3a29eaebd4
bostontecz <admin@bostontecz.com>
2026-03-30 08:28:18 +0400

    Update for PostgreAQL and Railway deployment

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'better-sqlite3',
+    client: 'postgres',
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: parseInt(env('DATABASE_PORT', '5432')),
+      database: env('DATABASE_NAME', 'slim'),
+      user: env('DATABASE_USER', 'user'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     useNullAsDefault: true,
   },
