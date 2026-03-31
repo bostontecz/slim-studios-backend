4ecff83a4dd2ce0222241dcf125aac50466a9ebf
bostontecz <admin@bostontecz.com>
2026-03-31 05:58:30 +0400

    Fix schema format for all content types

--- config/database.js
+++ config/database.js
@@ -2,12 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: parseInt(env('DATABASE_PORT', '5432')),
-      database: env('DATABASE_NAME', 'slim'),
-      user: env('DATABASE_USER', 'user'),
-      password: env('DATABASE_PASSWORD', 'password'),
-      ssl: env.bool('DATABASE_SSL', false),
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
     useNullAsDefault: true,
   },
